6019f1921470e79f0c4a8337d2005f8f7aeba645
Brendan Vandevoorde <vanbrec@dunwoody.edu>
2019-02-12 19:55:50 -0600

    clearing clutter and starting new a bit

--- Project1.0/Program.cs
+++ Project1.0/Program.cs
@@ -5,9 +5,10 @@ namespace Project1._0
 {
     class NFLDraft
     {
-        static void Main()
+        static void Main(string[] args)
         {
             List<string> SelectedPlayers = new List<string>();
+            
 
             string[,] Quarterbacks = {{"\t", "The Best", "\t2nd Best", "\t3rd Best", "\t4th Best", "\t5th Best\n"},
                                     {"Quarterback", "Dwayne Haskins", "\tKyler Murray", "\tDrew Lock", "\tDaniel Jones", "\tWill Grier"},
@@ -21,65 +22,33 @@ namespace Project1._0
                                 };
             int[,] Cost = { { 26400100, 20300100, 17420300, 13100145, 10300000 }, { 24500100, 19890200, 18700800, 15000000, 11600400 } };
 
+            int Cash = 95000000;
+
             outputTable(Quarterbacks);
             outputTable(Runningbacks);
-
-            bool done = false;
-            while (!done)
-            {
-
-
-
-
-                //Test loop to print answers
-                for (int x = 0; x < 10; ++x)
-                {
-                    string[] PlayerRow = new string[10];
-                    string[] PlayerColumn = new string[10];
-
-                    Console.WriteLine("\nEnter the Position number you want select a player from");
-                    PlayerRow[x] = Console.ReadLine();
-                    Console.WriteLine("\nEnter the Ranking number of the player you wish to select");
-                    PlayerColumn[x] = Console.ReadLine();
-                    string Test = new string(PlayerRow[x] + PlayerColumn[x]);
-
-                    bool PlayerSearch = SelectedPlayers.Contains(Test);
-                    if (PlayerSearch == false)
-                        SelectedPlayers.Add(Test);
-                    else
-                        Console.WriteLine("You have already selected this player, please select a different player");
-
-                    Console.WriteLine();
-                }
-                done = true;
-                
-
-                
-                
-
-            }
-
+            
 
 
         }
         static void outputTable(string[,] prod)
         {
-            
+
 
             for (var i = 0; i < prod.GetLength(0); i++)
             {
-                
+
                 for (var x = 0; x < prod.GetLength(1); x++)
                 {
                     Console.Write($"{prod[i, x]}\t");
                 }
                 Console.WriteLine("");
             }
-            
 
 
-            
+
+
         }
+
 
     }
 }
