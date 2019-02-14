bcb68804ef5b35dbf89b8123537ea5f652ac02c5
Brendan Vandevoorde <vanbrec@dunwoody.edu>
2019-02-14 16:05:03 -0600

    Table looks nice and neat now

--- Project1.0/Program.cs
+++ Project1.0/Program.cs
@@ -12,66 +12,66 @@ namespace Project1._0
 
             string[,] QuarterbackDisplay =
             {
-                {"\t", "The Best", "\t2nd Best", "\t3rd Best", "\t4th Best", "\t5th Best\n"},
-                {"Quarterback", "Dwayne Haskins", "\tKyler Murray", "\tDrew Lock", "\tDaniel Jones", "\tWill Grier"},
-                {"\t", "(Ohio St.)", "\t(Oklahoma)", "\t(Missouri)", "\t(Duke)", "\t\t(West Virginia)"},
-                {"\t", "$26,400,100", "\t$20,300,100", "\t$17,420,300", "\t$13,100,145", "\t$10,300,000"}
+                {"    Position\t", "1. The Best", "\t2. 2nd Best", "\t3. 3rd Best", "\t4. 4th Best", "\t5. 5th Best\n_________________________________________________________________________________________________________________________________________"},
+                {"1. Quarterback\t", "Dwayne Haskins", "\tKyler Murray", "\tDrew Lock", "\tDaniel Jones", "\tWill Grier"},
+                {"\t\t", "(Ohio St.)", "\t(Oklahoma)", "\t(Missouri)", "\t(Duke)", "\t\t(West Virginia)"},
+                {"\t\t", "$26,400,100", "\t$20,300,100", "\t$17,420,300", "\t$13,100,145", "\t$10,300,000\n_________________________________________________________________________________________________________________________________________"}
             };
 
 
             string[,] RunningbackDisplay =
             {
-                {"\nRunningback", "Josh Jacobs", "\tDamien Harris", "\tDavid Montgomery", "Justice Hill", "\tDevin Singletary"},
-                {"\t", "(Alabama)\t", "(Alabama)\t", "(Iowa St.)\t", "(Oklahoma St.)", "\t(FAU)   \t"},
-                {"\t", "$24,500,100", "\t$19,890,200", "\t$18,700,800", "\t$15,000,000", "\t$11,600,400\n"}
+                {"2. Runningback\t", "Josh Jacobs", "\tDamien Harris", "\tDavid Montgomery", "Justice Hill", "\tDevin Singletary"},
+                {"\t\t", "(Alabama)\t", "(Alabama)\t", "(Iowa St.)\t", "(Oklahoma St.)", "\t(FAU)   \t"},
+                {"\t\t", "$24,500,100", "\t$19,890,200", "\t$18,700,800", "\t$15,000,000", "\t$11,600,400\n_________________________________________________________________________________________________________________________________________"}
             };
 
 
             string[,] WideRecieverDisplay =
             {
-                {"\nWide-Reciever", "D.K. Metcalf", "A.J. Brown", "N'Keal Harry", "Marquise Brown", "Kelvin Harmon"},
-                {"\t", "(Ole Miss)", "(Ole Miss)", "(Arizona St.)", "(Oklahoma)", "(NC State)"},
-                {"\t", "$23,400,000", "\t$21,900,300", "\t$19,300,230", "\t$13,400,230", "\t$10,000,000\n"}
+                {"3. Wide-Reciever", "D.K. Metcalf\t", "A.J. Brown\t", "N'Keal Harry\t", "Marquise Brown\t", "Kelvin Harmon"},
+                {"\t\t", "(Ole Miss)\t", "(Ole Miss)\t", "(Arizona St.)\t", "(Oklahoma)\t", "(NC State)"},
+                {"\t\t", "$23,400,000", "\t$21,900,300", "\t$19,300,230", "\t$13,400,230", "\t$10,000,000\n_________________________________________________________________________________________________________________________________________"}
             };
 
 
             string[,] DeffensiveLinemanDisplay =
             {
-                {"\nDefensive Lineman", "Nick Bosa", "Quinnen Williams", "Ed Oliver", "Jeffery Simmons", "Clelin Ferrell"},
-                {"\t", "(Ohio St.)", "(Alabama)", "(Houston)", "(Miss. State)", "(Clemson)"},
-                {"\t", "$26,200,300", "\t$22,000,000", "\t$16,000,000", "\t$18,000,000", "\t$13,000,000\n"}
+                {"4. Defensive Lineman", "Nick Bosa", "\tQuinnen Williams", "Ed Oliver", "\tJeffery Simmons", "\tClelin Ferrell"},
+                {"\t\t", "(Ohio St.)", "\t(Alabama)", "\t(Houston)", "\t(Miss. State)", "\t(Clemson)"},
+                {"\t\t", "$26,200,300", "\t$22,000,000", "\t$16,000,000", "\t$18,000,000", "\t$13,000,000\n_________________________________________________________________________________________________________________________________________"}
             };
 
 
             string[,] DeffensiveBackDisplay =
             {
-                {"\nDefensive-Back", "Greedy Williams", "Deandre Baker", "Byron Murphy", "Deionte Thompson", "Amani Oruwariye"},
-                {"\t", "(LSU)", "(Georgia)", "(Washington)", "(Alabama)", "(Penn State)"},
-                {"\t", "$24,000,000", "\t$22,500,249", "\t$20,000,100", "\t$16,000,200", "\t$11,899,999\n"}
+                {"5. Defensive-Back", "Greedy Williams\t", "Deandre Baker\t", "Byron Murphy\t", "Deionte Thompson", "Amani Oruwariye"},
+                {"\t\t", "(LSU)\t\t", "(Georgia)\t", "(Washington)\t", "(Alabama)\t", "(Penn State)"},
+                {"\t\t", "$24,000,000", "\t$22,500,249", "\t$20,000,100", "\t$16,000,200", "\t$11,899,999\n_________________________________________________________________________________________________________________________________________"}
             };
 
 
             string[,] TightEndDisplay =
             {
-                {"\nTight Ends", "Noah Fant", "Irv Smith", "T.J. Hockenson", "Kaden Smith", "Josh Oliver"},
-                {"\t", "(Iowa)", "(Alabama)", "(Iowa)", "(Stanford)", "(San Jose St.)"},
-                {"\t", "$27,800,900", "\t$21,000,800", "\t$17,499,233", "\t$27,900,200", "\t$14,900,333\n"}
+                {"6. Tight Ends\t", "Noah Fant\t", "Irv Smith\t", "T.J. Hockenson\t", "Kaden Smith\t", "Josh Oliver"},
+                {"\t\t", "(Iowa)\t\t", "(Alabama)\t", "(Iowa)\t\t", "(Stanford)\t", "(San Jose St.)"},
+                {"\t\t", "$27,800,900", "\t$21,000,800", "\t$17,499,233", "\t$27,900,200", "\t$14,900,333\n_________________________________________________________________________________________________________________________________________"}
             };
 
 
             string[,] LineBackerDisplay =
             {
-                {"\nLine-Backer's", "Josh Allen", "Devin White", "Montez Sweat", "Mack Wilson", "Devin Bush"},
-                {"\t", "(Kentucky)", "(LSU)", "(Miss. State)", "(Alabama)", "(Michigan)"},
-                {"\t", "$22,900,300", "\t$19,000,590", "\t$18,000,222", "\t$12,999,999", "\t$10,000,100\n"}
+                {"7. Line-Backer's", "Josh Allen\t", "Devin White\t", "Montez Sweat\t", "Mack Wilson\t", "Devin Bush"},
+                {"\t\t", "(Kentucky)\t", "(LSU)\t\t", "(Miss. State)\t", "(Alabama)\t", "(Michigan)"},
+                {"\t\t", "$22,900,300", "\t$19,000,590", "\t$18,000,222", "\t$12,999,999", "\t$10,000,100\n_________________________________________________________________________________________________________________________________________"}
             };
 
 
             string[,] OffensiveTackleDisplay =
             {
-                {"\nOffensive Tackles", "Jonah Williams", "Greg Little", "Cody Ford", "Jawaan Taylor", "Yodny Cajuste"},
-                {"\t", "(Alabama)", "(Ole Miss)", "(Oklahoma)", "(Florida)", "(West Virginia)"},
-                {"\t", "$23,000,000", "\t$20,000,000", "\t$19,400,000", "\t$16,200,700", "\t$15,900,000\n"}
+                {"8. Offensive Tackles", "Jonah Williams\t", "Greg Little\t", "Cody Ford\t", "Jawaan Taylor\t", "Yodny Cajuste"},
+                {"\t\t", "(Alabama)\t", "(Ole Miss)\t", "(Oklahoma)\t", "(Florida)\t", "(West Virginia)"},
+                {"\t\t", "$23,000,000", "\t$20,000,000", "\t$19,400,000", "\t$16,200,700", "\t$15,900,000\n_________________________________________________________________________________________________________________________________________"}
             };
 
 
@@ -92,7 +92,7 @@ namespace Project1._0
             { 
                 { 26400100, 20300100, 17420300, 13100145, 10300000 },
                 { 24500100, 19890200, 18700800, 15000000, 11600400 },
-                { 3400000, 21900300, 19300230, 13400230, 10000000 },
+                { 23400000, 21900300, 19300230, 13400230, 10000000 },
                 { 26200300, 22000000, 16000000, 18000000, 13000000 },
                 { 24000000, 22500249, 20000100, 16000200, 11899999 },
                 { 27800900, 21000800, 17499233, 27900200, 14900333 },
@@ -121,6 +121,9 @@ namespace Project1._0
 
                         Console.Clear();
 
+
+                        Console.WriteLine("_________________________________________________________________________________________________________________________________________");
+
                         //Print Player Listing
                         outputTable(QuarterbackDisplay);
                         outputTable(RunningbackDisplay);
@@ -132,7 +135,7 @@ namespace Project1._0
                         outputTable(OffensiveTackleDisplay);
 
                         //Display Current Cash Available
-                        Console.WriteLine("Current Cash {0}\n", (Cash - totalCost).ToString("c"));
+                        Console.WriteLine("\t\t\t\t\t\t\t\t\t\t\t\t\t\t      Current Cash {0}\n", (Cash - totalCost).ToString("c"));
 
                         //Row Selection
                         position = PlayerPos();
