dbb1930dc9bc82e8ea99842005ea78ede6b98e38
Brendan Vandevoorde <vanbrec@dunwoody.edu>
2019-02-14 10:41:47 -0600

    Added Player Catch So The Same Player Cannot Be Selected Twice

--- Project1.0/Program.cs
+++ Project1.0/Program.cs
@@ -10,21 +10,38 @@ namespace Project1._0
             List<string> SelectedPlayers = new List<string>();
 
 
-            string[,] Quarterbacks = {{"\t", "The Best", "\t2nd Best", "\t3rd Best", "\t4th Best", "\t5th Best\n"},
-                                    {"Quarterback", "Dwayne Haskins", "\tKyler Murray", "\tDrew Lock", "\tDaniel Jones", "\tWill Grier"},
-                                    {"\t", "(Ohio St.)", "\t(Oklahoma)", "\t(Missouri)", "\t(Duke)", "\t\t(West Virginia)"},
-                                    {"\t", "$26,400,100", "\t$20,300,100", "\t$17,420,300", "\t$13,100,145", "\t$10,300,000"}
-                                };
-            string[,] Runningbacks = {
-                                    {"\nRunningback", "Josh Jacobs", "\tDamien Harris", "\tDavid Montgomery", "Justice Hill", "\tDevin Singletary"},
-                                    {"\t", "(Alabama)\t", "(Alabama)\t", "(Iowa St.)\t", "(Oklahoma St.)", "\t(FAU)   \t"},
-                                    {"\t", "$24,500,100", "\t$19,890,200", "\t$18,700,800", "\t$15,000,000", "\t$11,600,400\n"}
-                                };
+            string[,] QuarterbacksDisplay = 
+            {
+                {"\t", "The Best", "\t2nd Best", "\t3rd Best", "\t4th Best", "\t5th Best\n"},
+                {"Quarterback", "Dwayne Haskins", "\tKyler Murray", "\tDrew Lock", "\tDaniel Jones", "\tWill Grier"},
+                {"\t", "(Ohio St.)", "\t(Oklahoma)", "\t(Missouri)", "\t(Duke)", "\t\t(West Virginia)"},
+                {"\t", "$26,400,100", "\t$20,300,100", "\t$17,420,300", "\t$13,100,145", "\t$10,300,000"}
+            };
+
+
+            string[,] RunningbacksDisplay = 
+            {
+                {"\nRunningback", "Josh Jacobs", "\tDamien Harris", "\tDavid Montgomery", "Justice Hill", "\tDevin Singletary"},
+                {"\t", "(Alabama)\t", "(Alabama)\t", "(Iowa St.)\t", "(Oklahoma St.)", "\t(FAU)   \t"},
+                {"\t", "$24,500,100", "\t$19,890,200", "\t$18,700,800", "\t$15,000,000", "\t$11,600,400\n"}
+            };
+
+
+            string[,] SelectPlayerList =
+            {
+                {"Dwayne Haskins", "Kyler Murray", "Drew Lock", "Daniel Jones", "Will Grier"},
+                {"Josh Jacobs", "Damien Harris", "David Montgomery", "Justice Hill", "Devin Singletary"}
+            };
+
+
             int[,] Cost = { { 26400100, 20300100, 17420300, 13100145, 10300000 }, { 24500100, 19890200, 18700800, 15000000, 11600400 } };
+
 
             int Cash = 95000000;
 
+
             int totalCost = 0;
+
 
             ConsoleKey finished;
             finished = exit();
@@ -37,26 +54,27 @@ namespace Project1._0
 
                 Console.Clear();
 
-
-                Console.WriteLine("Current Cash {0}", Cash - totalCost);
-
                 //Print Player Listing
-                outputTable(Quarterbacks);
-                outputTable(Runningbacks);
+                outputTable(QuarterbacksDisplay);
+                outputTable(RunningbacksDisplay);
 
+                //Display Current Cash Available
+                Console.WriteLine("Current Cash {0}\n", Cash - totalCost);
 
+                //Row Selection
                 position = PlayerPos();
 
-
+                //Column Selection
                 rank = PlayerRank();
 
-                totalPrice(Quarterbacks, Cost, ref totalCost, position, rank);
+                //Take the cost of the player
+                totalPrice(SelectPlayerList, Cost, ref totalCost, position, rank, ref Cash, ref SelectedPlayers);
 
-                
+
+
             }
 
-
-
+            Console.WriteLine("\nThank You For Creating Your Team");
 
         }
 
@@ -85,7 +103,7 @@ namespace Project1._0
         {
             ConsoleKey sentinel;
 
-            Console.WriteLine("Press any key to start managing your team, press 'Shift + X' key once you are done to finish");
+            Console.WriteLine("Press any key to start managing your team, press 'Shift + X' key once you are done to finish\n");
             return sentinel = Console.ReadKey(true).Key;
         }
 
@@ -112,7 +130,7 @@ namespace Project1._0
         static int PlayerRank()
         {
             int rank; //Local
-            Console.WriteLine("Please enter the ranking number of the player you wish to add to your team");
+            Console.WriteLine("\nPlease enter the ranking number of the player you wish to add to your team");
             rank = Convert.ToInt32(Console.ReadLine()) - 1;
             
             if (rank < 0)
@@ -130,10 +148,29 @@ namespace Project1._0
             return rank;
         }
 
-        static void totalPrice(string[,] player, int[,] price, ref int total, int position, int rank)
+        static void totalPrice(string[,] player, int[,] price, ref int total, int position, int rank, ref int Cash, ref List<string> AddPlayer)
         {
-            Console.WriteLine($"You have selected {player[position, rank]} for {price[position, rank].ToString("c")}");
-            total += price[position, rank];
+
+            if ((Cash - (total + price[position, rank])) < 0)
+            {
+                Console.WriteLine("ERROR: Cash Will Reach Below Zero, This Player Cannot Be Selected");
+                Console.WriteLine("Please Press Enter And Select A Different Player");
+                Console.ReadLine();
+                total += 0;
+            }
+            else if (AddPlayer.Contains(player[position, rank]) == true)
+            {
+                Console.WriteLine("\n{0} Has Already Been Added To Your Team, Please Press 'Enter' And Select A Different Player", player[position, rank]);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine($"\nYou have selected {player[position, rank]} for {price[position, rank].ToString("c")}\nPress 'Enter' To Continue");
+                total += price[position, rank];
+                AddPlayer.Add(player[position, rank]);
+                Console.ReadLine();
+            }
+            
         }
 
     }
